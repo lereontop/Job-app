a1afa38a69d6a09d2859ffeba693cefc0a916f04
nurudeen olalere <lereontop>
2024-10-24 15:08:40 +0100

    first commit

---
+++ src/App2.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar/>
+  <Hero/>
+  <HomeCards/>
+  <JobListings :limit="3" :showButton="true"/>
+</template>
+
+<script setup>
+import Navbar  from './components/Navbar.vue';
+import Hero  from './components/Hero.vue';
+import HomeCards  from './components/HomeCards.vue';
+import JobListings  from './components/JobListings.vue';
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx }'],
+  theme: {
+    extend: {
+      fontFamily:{
+        sans:['Poppins', 'san-serif']
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 20%',
+      },
+    },
+  },
+  plugins: [],
+}
+
